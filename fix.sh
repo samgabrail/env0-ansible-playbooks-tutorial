d6d49aaaae55f3a4f3e84fd4e768b67eb23273e3
Sam Gabrail <sam.gabrail@tekanaid.com>
2024-09-19 11:33:48 -0400

    remove the fix script

--- fix.sh
+++
@@ -1,5 +0,0 @@
-cd ..
-rm -rf env0-ansible-playbooks-tutorial/
-git clone https://github.com/samgabrail/env0-ansible-playbooks-tutorial.git
-cd env0-ansible-playbooks-tutorial
-ansible-playbook -i inventory techcorp_playbook_advanced.yaml
